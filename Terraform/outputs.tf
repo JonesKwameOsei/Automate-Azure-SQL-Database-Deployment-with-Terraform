ac7086ebaba9f713cc7ea378de4bd09142ea6c3f
Jones Kwame Osei <oseikwamejones@gmail.com>
2024-04-30 02:53:22 +0100

    Changed resource name

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -1,7 +1,7 @@
-output "webapp_url" {
+output "dotnet_webapp_url" {
   value = azurerm_windows_web_app.app.default_hostname
 }
 
-output "webapp_ips" {
+output "dotnet_webapp_ips" {
   value = azurerm_windows_web_app.app.outbound_ip_addresses
 }
