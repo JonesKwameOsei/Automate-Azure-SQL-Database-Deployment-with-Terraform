c59bffa1dadd7c78d47a91baf34bc6ee76881616
Jones Kwame Osei <oseikwamejones@gmail.com>
2024-04-30 02:31:04 +0100

    modified terraform provider backend

--- Terraform/providers.tf
+++ Terraform/providers.tf
@@ -5,12 +5,12 @@ terraform {
       version = "~> 3.101.0"
     }
   }
-  backend "azurerm" {
-    resource_group_name     = "rg-terraform-state-001"
-    storage_account_name    = "cloudninjaterraformstate"
-    container_name          = "tfstate"
-    key                     = "GitHub-Terraform-rg-loganalytics-001"
-  }
+  # backend "azurerm" {
+  #   resource_group_name     = "rg-terraform-state-001"
+  #   storage_account_name    = "cloudninjaterraformstate"
+  #   container_name          = "tfstate"
+  #   key                     = "GitHub-Terraform-rg-loganalytics-001"
+  # }
 }
 
 
